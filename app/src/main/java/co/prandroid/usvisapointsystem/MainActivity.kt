2cc8d61702fea28eaf344af06dbae931e70129a7
dharmakshetri <dharma.kshetri@gmail.com>
2017-08-23 13:37:44 -0500

    added Crashlytics firebase with fabric

--- app/src/main/java/co/prandroid/usvisapointsystem/MainActivity.kt
+++ app/src/main/java/co/prandroid/usvisapointsystem/MainActivity.kt
@@ -3,12 +3,16 @@ package co.prandroid.usvisapointsystem
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Fabric.with(this, Crashlytics())
         val background = object : Thread() {
             override fun run() {
                 try {
